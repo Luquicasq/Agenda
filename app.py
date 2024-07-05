dd70f8fb583a29eef618a25d66af98d1509519cf
Luquicasq <lcasquero@fi.uba.ar.com>
2024-07-04 21:08:49 -0300

    Se creó nuevo endpoint en app.py. Ahora Index.html puede acceder a las tareas y mostrarlas en la grilla.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy #//sqlalchemy --> conexion DB
+from flask_sqlalchemy import SQLAlchemy #sqlalchemy --> conexion DB
 from flask_cors import CORS #CORS deja que se acceda al sv y DB
 from sqlalchemy import ForeignKey, Column, Integer, String, DateTime #Tipos de dato
 from sqlalchemy.orm import relationship #FKEY
@@ -31,10 +31,39 @@ class Agenda(db.Model):
     pendiente = db.Column(db.Boolean)
     fkt = db.Column(Integer, ForeignKey("tareas.id"))
     fkr = db.Column(Integer, ForeignKey("rutina.id"))
+    tarea = relationship('Tareas', foreign_keys=[fkt])
+    rutina = relationship('Rutina', foreign_keys=[fkr])
+
+@app.route('/agenda/', methods=['GET'])
+def conseguir_agenda():
+    pendientes = Agenda.query.filter_by(pendiente=True).all()
+    pendientes_dic = {}
+
+    for pendiente in pendientes:
+        if pendiente.tarea:
+            nombre = pendiente.tarea.nombre
+            hora = pendiente.tarea.hora
+            dia = pendiente.tarea.dia
+        elif pendiente.rutina:
+            nombre = pendiente.rutina.nombre
+            hora = pendiente.rutina.hora
+            dia = pendiente.rutina.dias
+
+
+        hora_inicio= hora.strip()
+
+        if dia not in pendientes_dic:
+            pendientes_dic[dia] = {}
+
+        if hora_inicio not in pendientes_dic[dia]:
+            pendientes_dic[dia][hora_inicio] = []
+
+        pendientes_dic[dia][hora_inicio].append(nombre)
+        
+    return jsonify(pendientes_dic)
 
 @app.route('/crear_tarea/', methods=['POST'])
 def crear_tarea():
-    print("aaa")
     if request.method == 'POST':
         nombre = request.form['nombre']
         descripcion = request.form['descripcion']
@@ -46,7 +75,7 @@ def crear_tarea():
             hora = f"{horainicio}-{horafin}"
 
         dias_form = request.form.getlist('dia')
-        dias = ', '.join(dias_form)
+        dias = '-'.join(dias_form)
 
         tarea = Tareas(nombre=nombre, descripcion=descripcion, dia=dias, hora=hora)
         db.session.add(tarea)
@@ -63,7 +92,6 @@ def crear_tarea():
 
 @app.route('/crear_rutina/', methods=['POST'])
 def crear_rutina():
-    print("aaa")
     if request.method == 'POST':
         nombre = request.form['nombre']
         descripcion = request.form['descripcion']
@@ -75,7 +103,7 @@ def crear_rutina():
             hora = f"{horainicio}-{horafin}"
 
         dias_form = request.form.getlist('dia')
-        dias = ', '.join(dias_form)
+        dias = '-'.join(dias_form)
 
         rutina = Rutina(nombre=nombre, descripcion=descripcion, dias=dias, hora=hora)
         db.session.add(rutina)
